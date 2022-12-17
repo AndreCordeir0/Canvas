ff079313cb954827dd9caece70dbe5c348183f39
Your Name <you@example.com>
2022-12-16 22:13:26 -0300

    ajustes

--- index.js
+++ index.js
@@ -8,23 +8,27 @@ window.onload = animation();
 
 function animation(){
 
-    let image = new Image();
-    image.src = 'assets/BODY_animation.png';
 
-    setInterval(() => {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
         let image = new Image();
         image.src = 'assets/BODY_animation.png';
        
         console.log(inicioRecorte);
         image.onload = () =>{
-            ctx.drawImage(image,inicioRecorte, 0,64,64,0,0,64,64);
+
+            setInterval(() => {
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+                ctx.drawImage(image,inicioRecorte, 0,64,64,0,0,64,64);
+                inicioRecorte += image.width / 8 ;
+                if(inicioRecorte > 384 ){
+                    inicioRecorte = 0;
+                }
+            }, 100);
+         
         }
-        inicioRecorte += image.width / 8 ;
-        if(inicioRecorte > 384 ){
-            inicioRecorte = 0;
-        }
-    }, 100);
+
+
     
 }
 
